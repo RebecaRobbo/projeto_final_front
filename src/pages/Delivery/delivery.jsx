ca905b1f16e4d230f596899f54c723ff04f02d5d
Rebeca Robbo <rebeca.lqa@gmail.com>
2023-03-30 21:30:08 -0300

    Validação da hora

--- src/pages/Delivery/delivery.jsx
+++ src/pages/Delivery/delivery.jsx
@@ -10,7 +10,6 @@ export const Delivery = () => {
         document.title = 'Delivery';
         const horario = localStorage.getItem("horario");
         setHorarios(JSON.parse(horario) || []);
-        console.log(produto);
         setProdutos(JSON.parse(produto) || []);
     }, [produto]);
 
@@ -20,16 +19,22 @@ export const Delivery = () => {
     var produtosLength = produtos.length;
 
     while (produtosLength >= cont) {
-        console.log(produtos[cont]);
+        var prd = produtos[cont]
+        var strPrd = JSON.stringify(prd);
         cont += 1;
     }
-
     var data = new Date();
-    console.log(data);
-    var hora = data.getHours;
-    var minuto = data.getMinutes
-    
-
+    var horaAtual = parseInt(data.toLocaleTimeString());
+    var horaEscolhida = parseInt(horarios.horas);
+    var calculaHora = 0;
+    if(horaAtual > horaEscolhida){
+        alert("Já passou do horário de retirada do seu pedido");
+    }else if(horaEscolhida > horaAtual){
+        calculaHora = horaEscolhida - horaAtual;
+        console.log(calculaHora);
+    }else if(horaAtual === horaEscolhida){
+        alert("A hora para retirar o seu pedido está perta, fique atendo!");
+    }
         return (
             <>
                 <div className="top-time">
@@ -58,4 +63,6 @@ export const Delivery = () => {
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d234114.4115905254!2d-47.12383556718747!3d-23.531147099999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cf01460b9de951%3A0x24f3f9a2e1457b5a!2sZilda%20Salgados!5e0!3m2!1spt-BR!2sbr!4v1668815845579!5m2!1spt-BR!2sbr"></iframe>
             </>
         );
+
+    
     }

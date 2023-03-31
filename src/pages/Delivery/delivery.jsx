c5625328264e4cb49937905ac15d25e2c0bd94ac
Rebeca Robbo <rebeca.lqa@gmail.com>
2023-03-30 21:47:43 -0300

    Ajuste estetico horas

--- src/pages/Delivery/delivery.jsx
+++ src/pages/Delivery/delivery.jsx
@@ -6,9 +6,10 @@ export const Delivery = () => {
     const [horarios, setHorarios] = useState([]);
     const [produtos, setProdutos] = useState([]);
     var produto = localStorage.getItem("produtos");
+    var horario = localStorage.getItem("horario");
     useEffect(() => {
         document.title = 'Delivery';
-        const horario = localStorage.getItem("horario");
+        
         setHorarios(JSON.parse(horario) || []);
         setProdutos(JSON.parse(produto) || []);
     }, [produto]);
@@ -63,6 +64,6 @@ export const Delivery = () => {
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d234114.4115905254!2d-47.12383556718747!3d-23.531147099999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cf01460b9de951%3A0x24f3f9a2e1457b5a!2sZilda%20Salgados!5e0!3m2!1spt-BR!2sbr!4v1668815845579!5m2!1spt-BR!2sbr"></iframe>
             </>
         );
-
+        
     
     }
